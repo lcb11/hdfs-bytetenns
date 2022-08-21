153c41ebb46686fd2ca5b7d191fd14267bd40409
lcb11 <2990024235@qq.com>
2022-08-21 22:20:11 +0800

    修改一些包名引用错误

--- hdfs-backupnode/src/main/java/com/bytetenns/backupnode/fs/FsDirectory.java
+++ hdfs-backupnode/src/main/java/com/bytetenns/backupnode/fs/FsDirectory.java
@@ -5,7 +5,7 @@ import com.bytetenns.dfs.model.backup.INode;
 import com.bytetenns.common.enums.NodeType;
 import com.bytetenns.common.netty.Constants;
 import com.bytetenns.common.utils.StringUtils;
-import com.ruyuan.dfs.model.namenode.Metadata;
+import com.bytetenns.dfs.model.namenode.Metadata;
 import lombok.extern.slf4j.Slf4j;
 import java.util.*;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
